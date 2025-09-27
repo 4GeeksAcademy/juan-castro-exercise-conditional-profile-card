a1a77bce8efb79188869d2a311a775778fe0776e
juan <130606010+juancastrop@users.noreply.github.com>
2025-09-27 00:18:53 +0000

    actualizando con ternarios

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,24 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName} </h1>
+          <h1>${variables.name === null ? "" : variables.name} ${
+    variables.lastName === null ? "" : variables.lastName
+  } </h1>
           <h2>${variables.role} </h2>
           <h3>${variables.country} ${variables.city} </h3>
           <ul class= ${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/4geeksacademy">${variables.twitter} <i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy">${variables.github}<i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy">${variables.linkedin}<i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy">${variables.instagram}<i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy">${
+              variables.twitter
+            } <i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy">${
+              variables.github
+            }<i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy">${
+              variables.linkedin
+            }<i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy">${
+              variables.instagram
+            }<i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
